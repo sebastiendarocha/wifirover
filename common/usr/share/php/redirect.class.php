ec845910f5e2dc1c4f7056456c6def3b52bf4608
Loic <zgalki@opmbx.org>
2017-05-22 19:00:49 +0200

    add cloudweblib and weblib plugins

--- common/usr/share/php/redirect.class.php
+++ common/usr/share/php/redirect.class.php
@@ -2,7 +2,7 @@
 
 include_once("redirect_plugin.class.php");
 include_once("plugable.class.php");
-
+include_once('common.php');
 
 class redirect extends plugable {
     function getListIgnoreSites()
@@ -12,7 +12,7 @@ class redirect extends plugable {
 
     function getUrlRedirect()
     {
-        return $this->call_plugin("redirect_default", "getUrlRedirect");
+        return $this->call_plugin( getValueFromConf("/etc/wifi_rover.conf", 'PORTALMODE'), "getUrlRedirect");
     }
 
     function getName() { return __CLASS__; }
